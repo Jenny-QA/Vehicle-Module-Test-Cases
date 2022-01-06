d79983136748175a555525c9e9257347bc10f044
Jenny-QA <jennyrat1997@gmail.com>
2022-01-06 17:10:47 +0530

    Commit Changes

--- VehicleTestCases.com/src/test/java/TestCases/StopSellMasterTestCases.java
+++ VehicleTestCases.com/src/test/java/TestCases/StopSellMasterTestCases.java
@@ -28,15 +28,15 @@ public class StopSellMasterTestCases {
 	
 	@BeforeSuite
 	public void initDriver() {
-		/*WebDriverManager.chromedriver().setup();
+		WebDriverManager.chromedriver().setup();
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--no-sandbox");
 		options.addArguments("--disable-dev-shm-usage");
 		options.addArguments("--window-size=1920,1080");
 		options.addArguments("--headless");
-		driver = new ChromeDriver(options);*/
-		WebDriverManager.firefoxdriver().setup();
-		driver = new FirefoxDriver();
+		driver = new ChromeDriver(options);
+//		WebDriverManager.firefoxdriver().setup();
+//		driver = new FirefoxDriver();
 	}
 	
 	
