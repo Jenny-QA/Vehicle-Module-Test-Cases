32a58acf8d04f5ad58f41e602525376baf06ab2a
Jenny-QA <jennyrat1997@gmail.com>
2022-01-19 14:53:55 +0530

    API Response Update

--- VehicleTestCases.com/src/test/java/TestCases/APIResponse.java
+++ VehicleTestCases.com/src/test/java/TestCases/APIResponse.java
@@ -17,6 +17,72 @@ public class APIResponse {
 	public static final String vehicleAdd = "Vehicle Type added successfully";
 	public static final String vehicleExists = "Vehicle Type already exists";
 	public static final String vehicleUpdate = "Vehicle Type updated successfully";
+	public static final String vehicleDelete = "Vehicle Type deleted successfully";
+	public static final String vehicleDeleteAlready = "Vehicle Type is already deleted";
+	
+	//Sub Vehicle Master Messages
+	public static final String subvehicleMasterSuccess = "Vehicle Type get successfully";
+	public static final String subvehicleAdd = "Vehicle Type added successfully";
+	public static final String subvehicleExists = "Vehicle Type already exists";
+	public static final String subvehicleUpdate = "Vehicle Type updated successfully";
+	public static final String subvehicleDelete = "Vehicle Type deleted successfully";
+	public static final String subvehicleDeleteAlready = "Vehicle Type is already deleted";
+	
+	//Vehicle Manufacturer Messages
+	public static final String manufacturerSuccess = "Vehicle Manufacturer get successfully";
+	public static final String manufacturerAdd = "Vehicle Manufacturer added successfully";
+	public static final String manufacturerExists = "Manufacturer already exists";
+	public static final String manufacturerUpdate = "Vehicle Manufacturer updated successfully";
+	public static final String manufacturerDelete = "Vehicle manufacturer deleted successfully";
+	public static final String manufacturerDeleteAlready = "Vehicle manufacturer is already deleted";
+	
+	//Vehicle Color Master Messages
+	public static final String colorSuccess = "Vehicle Color get successfully";
+	public static final String colorAdd = "Vehicle Color added successfully";
+	public static final String colorNameExists = "Vehicle Color Name already exists";
+	public static final String colorCodeExists = "Vehicle Color code already exists";
+	public static final String colorDelete = "Vehicle Color deleted successfully";
+	public static final String colorDeleteAlready = "Vehicle Color is already deleted";
+	
+	//Fuel Master Messages
+	public static final String fuelSuccess = "Fuel Type get successfully";
+	public static final String fuelAdd = "Fuel Type added successfully";
+	public static final String fuelExists = "Fuel Type already exists";
+	public static final String fuelUpdate = "Fuel Type updated successfully";
+	public static final String fuelDelete = "Fuel Type deleted successfully";
+	public static final String fuelDeleteAlready = "Fuel Type is already deleted";
+	
+	//Pickup Type Master Messages
+	public static final String pickupSuccess = "";
+	public static final String pickupAdd = "";
+	public static final String pickupExists = "";
+	public static final String pickupUpdate = "";
+	public static final String pickupDelete = "";
+	public static final String pickupDeleteAlredy = "";
+	
+	//Promotion Master Messages
+	public static final String promotionSuccess = "";
+	public static final String promotionAdd = "";
+	public static final String promotionExists = "";
+	public static final String promotionUpdate = "";
+	public static final String promotionDelete = "";
+	public static final String promotionDeleteAlready = "";
+	
+	//Billing Type Messages
+	public static final String billingSuccess = "";
+	public static final String billingAdd = "";
+	public static final String billingExists = "";
+	public static final String billingUpdate = "";
+	public static final String billingDelete = "";
+	public static final String billingDeleteAlready = "";
+	
+	//Weight unit Messages
+	public static final String unitSuccess = "";
+	public static final String unitAdd = "";
+	public static final String unitExists = "";
+	public static final String unitUpdate = "";
+	public static final String unitDelete = "";
+	public static final String UnitDeleteAlready = "";
 	
 	
 }
